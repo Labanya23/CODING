fcb47868e2f7d5197b1f93120ca629bcb41f914b
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2023-11-17 21:44:19 +0600

    Update 250 Thousand Tons of TNT.cpp

--- 250 Thousand Tons of TNT.cpp
+++ 250 Thousand Tons of TNT.cpp
@@ -36,3 +36,44 @@ int main()
 
     return 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long n,m=0;
+        cin>>n;
+        long long a[n];
+        cin>>a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        int ma=a[0],mi=a[0];
+        //int ma,mi;
+        for(int i=0;i<n;i++){
+        if(a[i]>ma){
+            ma=a[i];
+        }
+        if(a[i]<mi){
+            mi=a[i];
+        }
+         m=ma-mi;
+        }
+        //int m=ma-mi;
+        cout<<m<<endl;
+
+
+        //int m=max(a[i],a[i+1]);
+        //int n=min(a[i],a[i+1]);
+    }
+    //int r=m-n;
+    //cout<<r<<endl;
+
+
+    return 0;
+}
+
